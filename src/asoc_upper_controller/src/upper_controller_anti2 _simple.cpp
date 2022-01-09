dcca35f8d7649ff0d4f0d8c26aa162ada0f26a85
yinsiyuan1 <183373558@qq.com>
2022-01-09 21:31:26 +0800

    添加测试imu&悬挂的简版代码

---
+++ src/asoc_upper_controller/src/upper_controller_anti2 _simple.cpp
@@ -0,0 +1,68 @@
+#include "include/upper_controller_simple.hpp"
+//配合 upper_controller_simple.h的一个imu&悬挂测试
+
+double zero_pos, P_pit, D_pit, P_rol, D_rol, last_pitch, last_roll;
+
+UpperController::UpperController()
+{
+  // Private parameters handler
+  ros::NodeHandle pn("~");
+
+  // Controller parameter
+  pn.param("P_pit", P_pit, 150.0);
+  pn.param("D_pit", D_pit, 1.0);
+  pn.param("P_rol", P_rol, 150.0);
+  pn.param("D_rol", D_rol, 1.0);
+
+  // Publishers and Subscribers
+  imu_sub = n_.subscribe("/imu_data_rpy0", 1, &UpperController::imuCB, this);
+  pub_suspension = n_.advertise<geometry_msgs::PolygonStamped>("/suspension_cmd", 1);
+
+  // Timer
+  timer1 = n_.createTimer(ros::Duration((1.0) / 20),
+                          &UpperController::controlLoopCB,
+                          this); // Duration(0.05) -> 20Hz
+}
+
+void UpperController::controlLoopCB(const ros::TimerEvent &)
+{
+  std_msgs::Float32MultiArray imuMsg = imu_msg;
+  susp_cmd.polygon.points.resize(4);
+
+  double roll = imuMsg.data[0];  // ego roll
+  double pitch = imuMsg.data[1]; // ego pitch
+  double yaw = imuMsg.data[2];
+
+  // body control
+  susp_cmd.polygon.points[0].x = P_pit * pitch + D_pit * (pitch - last_pitch);
+  susp_cmd.polygon.points[1].x = -(P_rol * roll + D_rol * (roll - last_roll));
+  susp_cmd.polygon.points[2].x = -(P_pit * pitch + D_pit * (pitch - last_pitch));
+  susp_cmd.polygon.points[3].x = P_rol * roll + D_rol * (roll - last_roll);
+
+  last_pitch = pitch;
+  last_roll = roll;
+
+  // limit max values
+  for (int i = 0; i < 4; i++)
+  {
+    susp_cmd.polygon.points[i].x = fmin(fmax(susp_cmd.polygon.points[i].x, -8.0), 8.0);
+  }
+
+  ROS_INFO("----------");
+  ROS_INFO("Roll:%.2f, Pitch:%.2f,Yaw: %.2f", roll, pitch, yaw);
+
+  susp_cmd.header.stamp = ros::Time::now();
+  pub_suspension.publish(susp_cmd);
+}
+
+/*****************/
+/* MAIN FUNCTION */
+/*****************/
+int main(int argc, char **argv)
+{
+  // Initiate ROS
+  ros::init(argc, argv, "UpperController");
+  UpperController controller;
+  ros::spin();
+  return 0;
+}
